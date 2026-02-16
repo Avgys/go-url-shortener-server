863a0726c7cdf69267b94864695f52940e2699a4
illia kosykh <illia.kosykh@mego.travel>
2026-02-16 20:02:22 +0300

    iter11 add migrations

--- internal/repository/dbstore.go
+++ internal/repository/dbstore.go
@@ -7,7 +7,11 @@ import (
 
 	"github.com/Avgys/go-url-shortener-server/internal/model"
 	"github.com/Avgys/go-url-shortener-server/internal/repository/db"
+	"github.com/golang-migrate/migrate/v4"
 	"github.com/jackc/pgx/v5"
+
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
 type DBStore struct {
@@ -15,6 +19,11 @@ type DBStore struct {
 }
 
 func NewDBStore(ctx context.Context, dbConfig *db.Config) (*DBStore, error) {
+
+	if err := runMigrations(dbConfig); err != nil {
+		return nil, err
+	}
+
 	dbConnection, err := db.NewDB(ctx, dbConfig)
 
 	if err != nil {
@@ -22,6 +31,23 @@ func NewDBStore(ctx context.Context, dbConfig *db.Config) (*DBStore, error) {
 	}
 
 	return &DBStore{db: dbConnection}, nil
+}
+
+func runMigrations(cfg *db.Config) error {
+	m, err := migrate.New(
+		"file://../../migrations/",
+		cfg.ConnectionString,
+	)
+
+	if err != nil {
+		return fmt.Errorf("couldn't open migrations, %w", err)
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return fmt.Errorf("couldn't run migrations, %w", err)
+	}
+
+	return nil
 }
 
 func (s *DBStore) StoreURL(ctx context.Context, fullURL string, shortURL string) error {
